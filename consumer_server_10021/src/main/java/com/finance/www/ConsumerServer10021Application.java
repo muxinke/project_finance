f5d796def62184805dfcc5cfb8e85b206eacb108
git <415429746@qq.com>
2019-07-23 17:39:02 +0800

    Initial commit

---
+++ consumer_server_10021/src/main/java/com/finance/www/ConsumerServer10021Application.java
@@ -0,0 +1,15 @@
+package com.finance.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerServer10021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerServer10021Application.class, args);
+	}
+
+}
