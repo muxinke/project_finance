96c0ca389ab2ea00855156eddda41c81eff26ece
git <415429746@qq.com>
2019-07-27 20:58:16 +0800

    穆歆珂

---
+++ provider_server_10001/src/main/java/com/finance/www/service/impl/MemberAccountServiceImpl.java
@@ -0,0 +1,11 @@
+package com.finance.www.service.impl;
+
+import com.finance.www.service.MemberAccountService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2019/7/25.
+ */
+@Service
+public class MemberAccountServiceImpl implements MemberAccountService {
+}
