77fae149ded2efd318da75f6ac007cda0b937b06
DYF <18603806152@163.com>
2019-07-28 16:48:38 +0800

    添加.gitignore文件i

---
+++ oauth_server_8050/src/test/java/com/finance/www/oauth_server_8050/OauthServer8050ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.finance.www.oauth_server_8050;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OauthServer8050ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
